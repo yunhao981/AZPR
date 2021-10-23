393908af2734b4b7e7573f766c08619902232286
Yunhao Liu <yunhao981@gmail.com>
2021-10-24 02:03:07 +0800

    updated macro files

--- global_config.h
+++ global_config.h
@@ -17,13 +17,25 @@
 
     `ifdef POSITIVE_RESET
         `define RESET_EDGE posedge
-        `define RESET_ENABLE 
+        `define RESET_ENABLE 1'b1
+	    `define RESET_DISABLE 1'b0
     `endif
 
     `ifdef NEGATIVE_RESET        
         `define RESET_EDGE negedge
+	    `define RESET_ENABLE 1'b0
+	    `define RESET_DISABLE 1'b1
     `endif
 
+    `ifdef POSITIVE_MEMORY
+        `define MEM_ENABLE 1'b1
+        `define MEM_DISABLE 1'b0
+    `endif 
+
+    `ifdef NEGATIVE_MEMORY
+        `define MEM_ENABLE 1'b0
+        `define MEM_DISABLE 1'b1
+    `endif 
 `endif
         
 
